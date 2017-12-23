82b110e3d45be72caabddca977aad408d368546e
Achmad Mahardi <achmad@mahardi.me>
2017-12-23 08:53:25 +0700

    Use clustering

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const express = require('express')
+const express = require('express');
+const cluster = require('cluster');
 
 const app = express();
 
 const {getPinnedRepos} = require('./src');
 
 const PORT = process.env.PORT || 3001;
+const CONCURRENCY = process.env.WEB_CONCURRENCY || 1;
 
-app.get('/:username', (req, res) => {
-    const username = req.params.username;
-    getPinnedRepos(username).then(data => {
-        console.log(data);
-        return res.json(data);
-    }).catch(err => {
-        console.error(err);
-        return res.json({
-            message: `Cannot pull pinned repos: ${err.message}`,
-        });
-    })
-})
-
-app.listen(PORT, (err) => {
-    if (err) {
-        console.error(`Cannot run server on port ${PORT}`)
-    } else {
-        console.error(`Server running on ${PORT}`)
+if (cluster.isMaster) {
+    for (let i = 0; i < CONCURRENCY; i+= 1) {
+        cluster.fork();
     }
-})
+
+    cluster.on('online', worker => {
+        console.log(`Worker ${worker.id} is online.`);
+    })
+
+    cluster.on('exit', worker => {
+        console.log(`Worker ${worker.id} died. Replacing...`);
+        cluster.fork();
+    })
+} else {
+    app.get('/:username', (req, res) => {
+        const username = req.params.username;
+        getPinnedRepos(username).then(data => res.json(data)).catch(err => {
+            console.error(err);
+            return res.json({
+                message: `Cannot pull pinned repos: ${err.message}`,
+            });
+        })
+    })
+    
+    app.listen(PORT, err => {
+        if (err) {
+            console.error(`Cannot run server on port ${PORT}`)
+        }
+    })    
+}
